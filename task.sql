a93523a6e69286922d2c8620f1bd8d1a16535769
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-07-15 16:38:32 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,6 +1,13 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
 
+DROP INDEX IF EXISTS Email ON Customers;
+DROP INDEX IF EXISTS Name ON Products;
+DROP INDEX IF EXISTS CustomerID ON Orders;
+DROP INDEX IF EXISTS OrderID ON OrderItems;
+DROP INDEX IF EXISTS ProductID ON OrderItems;
+DROP INDEX IF EXISTS Date ON Orders;
+
 CREATE INDEX Email ON Customers(Email);
 
 CREATE INDEX Name ON Products(Name);
@@ -10,5 +17,6 @@ CREATE INDEX CustomerID ON Orders(CustomerID);
 CREATE INDEX OrderID ON OrderItems(OrderID);
 
 CREATE INDEX ProductID ON OrderItems(ProductID);
+
 
 CREATE INDEX Date ON Orders(Date);
