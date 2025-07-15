efbbff41a777a5a42041dd47a80de986e3944097
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-07-15 16:35:10 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,14 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
+
+CREATE INDEX Email ON Customers(Email);
+
+CREATE INDEX Name ON Products(Name);
+
+CREATE INDEX CustomerID ON Orders(CustomerID);
+
+CREATE INDEX OrderID ON OrderItems(OrderID);
+
+CREATE INDEX ProductID ON OrderItems(ProductID);
+
+CREATE INDEX Date ON Orders(Date);
